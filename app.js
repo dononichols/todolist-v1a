d4b016595eec4228d608bb19d151816957e42d94
Meraki_DNA <dononichols@gmail.com>
2022-07-22 16:28:58 -0400

    -add res.sendFile=to send html page

--- app.js
+++ app.js
@@ -10,12 +10,10 @@ app.get("/", function(req, res) {
     var today = new Date();
     var currentDay = today.getDay();
 
-    if(currentDay === 5 || currentDay === 0) {
+    if(currentDay === 8 || currentDay === 0) {
         res.write("<h1>Yay it's the weekend!</h1>");
     } else {
-        res.write("<p>It's not the weekend</p>");
-        res.write("<h1>Boo! I have to work!</h1>");
-        res.send();
+        res.sendFile(__dirname + "/index.html");
     }
 
 });
